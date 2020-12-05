96f2f08e9cae9b0107f3402d04b203dcf1ad73c4
Taylor <hinestaylor29@gmail.com>
2020-12-05 13:37:14 -0800

    added resume

--- src/Resume.js
+++ src/Resume.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Taylor_Hines_Resume from "./Taylor_Hines_Resume.pdf";
 import "./Resume.css";
 
 export default function Resume(){
     return(
         <div className="resume" id="resume">
            <h1 className="section-title"> RESUME </h1>
-           <h6 className="resume-description"> Check out my resume  <a className="resume-link" href="/"> here </a> </h6>
+           <h6 className="resume-description"> Check out my resume  <a className="resume-link" href={Taylor_Hines_Resume}> here </a> </h6>
         </div>
     );
 }
