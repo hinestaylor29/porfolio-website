e930283a43b90a6087e45d6d0cb2380914a108de
Taylor <hinestaylor29@gmail.com>
2020-12-05 23:05:18 -0800

    styling with font awesome

--- src/Contact.js
+++ src/Contact.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "./Contact.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { faBehance } from '@fortawesome/free-brands-svg-icons';
 
 export default function Contact(){
     return(
         <div className="contact" id="contact">
         <h1 className="section-title"> CONTACT </h1>
         <ul className="contact-links">
-            <li> <a id="link" href="https://www.linkedin.com/in/taylor-hines/" rel="noreferrer" target="_blank"> LinkedIn </a></li>
-            <li> <a id="link" href="https://www.behance.net/taylorhines2?isa0=1" rel="noreferrer" target="_blank"> Behance </a></li>
-            <li> <a id="link" href="mailto:hinestaylor29@gmail.com"> Email Me </a></li>
+            <li> <a id="link" href="https://www.linkedin.com/in/taylor-hines/" rel="noreferrer" target="_blank"> <FontAwesomeIcon className="fa-2x" icon={faLinkedinIn} /> </a></li>
+            <li> <a id="link" href="https://www.behance.net/taylorhines2?isa0=1" rel="noreferrer" target="_blank"> <FontAwesomeIcon className="fa-2x" icon={faBehance} /> </a></li>
+            <li> <a id="link" className="email" href="mailto:hinestaylor29@gmail.com"> Email Me </a></li>
         </ul>
 
         <footer>
--- src/Resume.js
+++ src/Resume.js
@@ -6,7 +6,7 @@ export default function Resume(){
     return(
         <div className="resume" id="resume">
            <h1 className="section-title"> RESUME </h1>
-           <h6 className="resume-description"> Check out my resume  <a className="resume-link" href={Taylor_Hines_Resume}> here </a> </h6>
+           <h6 className="resume-description"> Peruse my resume  <a className="resume-link" href={Taylor_Hines_Resume}> here </a> </h6>
         </div>
     );
 }
