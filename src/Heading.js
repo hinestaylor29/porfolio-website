b23fcf549e0dd2e9f7504732cb9f3ce2e6832762
Taylor <hinestaylor29@gmail.com>
2020-12-13 12:54:38 -0800

    removed favicon

--- src/Heading.js
+++ src/Heading.js
@@ -5,7 +5,7 @@ export default function Heading(){
 return(
     <div className="col-12 name-title">
         <h1 className="name"> TAYLOR HINES </h1>
-        <p className="title"> UX Designer </p>
+        <p className="title"> Junior UX Designer </p>
         <p> Currently working at Dolby Labs in San Fransico. </p>
     </div>
 );
